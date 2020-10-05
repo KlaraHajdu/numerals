0bd613a1ebea977645a82a6b5a49c47baf15866a
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-05 21:31:27 +0200

    Implement conversion until 12

--- src/util.js
+++ src/util.js
@@ -1,10 +1,37 @@
-
 const convertNumbers = (input) => {
-
-    if (input === "1") return ("one");
     
-    return "";
+    let inputNumber = parseInt(input);
+    switch (inputNumber) {
+        case 0:
+            return "zero";
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        case 10:
+            return "ten";
+        case 11:
+            return "eleven";
+        case 12:
+            return "twelve";
 
-}
+    }
+
+    return "";
+};
 
 export default convertNumbers;
