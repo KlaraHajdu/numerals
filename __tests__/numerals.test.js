346865f36d737798ec047af27d0689fe6793da56
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-05 20:49:08 +0200

    Add 1 to one integration test

--- __tests__/numerals.test.js
+++ __tests__/numerals.test.js
@@ -1,3 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NumberConversion from "../src/components/NumberConversion";
+
 it("smoke test", () => {
     expect(true).toEqual(true);
- });
+});
+
+it("provides one for 1", () => {
+    const { getByTestId } = render(<NumberConversion />);
+   
+    const inputNode = screen.getByLabelText(/Input text:/i)
+    userEvent.type(inputNode, '1');
+
+    userEvent.click(screen.getByText('Submit'))
+
+    expect(getByTestId("number-text").textContent).toEqual("one");
+});
