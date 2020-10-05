2f048ea55fd2a54886ce4e13daf2546d03200939
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-05 21:32:07 +0200

    Add test thirteen for 13

--- __tests__/numerals.test.js
+++ __tests__/numerals.test.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import NumberConversion from "../src/components/NumberConversion";
+
+afterEach(cleanup)
 
 it("smoke test", () => {
     expect(true).toEqual(true);
@@ -27,4 +29,15 @@ it("provides two for 2", () => {
     userEvent.click(screen.getByText('Submit'))
 
     expect(getByTestId("number-text").textContent).toEqual("two");
+});
+
+it("provides thirteen for 13", () => {
+    const { getByTestId } = render(<NumberConversion />);
+   
+    const inputNode = screen.getByLabelText(/Input text:/i)
+    userEvent.type(inputNode, '13');
+
+    userEvent.click(screen.getByText('Submit'))
+
+    expect(getByTestId("number-text").textContent).toEqual("thirteen");
 });
