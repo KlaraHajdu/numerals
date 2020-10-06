b9a3b3098643943fce1a3d6605672fe78377b1b8
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-06 09:51:59 +0200

    Refactor conversion

--- src/util.js
+++ src/util.js
@@ -1,35 +1,11 @@
+import numbersObject from "./numbersObject";
+
 const convertNumbers = (input) => {
     
     let inputNumber = parseInt(input);
-    switch (inputNumber) {
-        case 0:
-            return "zero";
-        case 1:
-            return "one";
-        case 2:
-            return "two";
-        case 3:
-            return "three";
-        case 4:
-            return "four";
-        case 5:
-            return "five";
-        case 6:
-            return "six";
-        case 7:
-            return "seven";
-        case 8:
-            return "eight";
-        case 9:
-            return "nine";
-        case 10:
-            return "ten";
-        case 11:
-            return "eleven";
-        case 12:
-            return "twelve";
 
-    }
+    if (numbersObject[inputNumber]) return numbersObject[inputNumber]
+
 
     return "";
 };
