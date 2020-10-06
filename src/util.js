d8d88b62f8f1e84b07f8d38eb27fde101f59a1fd
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-10-06 11:29:26 +0200

    Implement conversion until 99

--- src/util.js
+++ src/util.js
@@ -2,12 +2,34 @@ import numbersObject from "./numbersObject";
 
 const convertNumbers = (input) => {
     
-    let inputNumber = parseInt(input);
+    let inputNumber = Number(input);
 
-    if (numbersObject[inputNumber]) return numbersObject[inputNumber]
+    if (!Number.isInteger(inputNumber)) return "";
 
+    if (numbersObject[1][inputNumber]) return numbersObject[1][inputNumber];
+    else if (numbersObject['special'][inputNumber]) return numbersObject['special'][inputNumber];
 
-    return "";
+    else {
+
+        let numberTextArr = []; 
+        
+        for (let i = 1; i <= input.length; i++){
+
+            let digit = input.charAt(input.length-i);
+            console.log(digit)
+            
+            if (i === 1) numberTextArr.push(numbersObject[i][digit])
+
+            if (i === 2) {
+                let tens = parseInt(digit.toString() + 0);
+                console.log(tens);
+                numberTextArr.push(numbersObject[i][tens])
+            }
+        }
+        let result = numberTextArr.reverse().join("-");
+        return result;
+    }
+
 };
 
 export default convertNumbers;
